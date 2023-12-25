99018236da131be41396e7cb1c8f62424c7f4e1f
l9o <ertye9020@gmail.com>
2023-12-25 20:10:01 +0500

    I solved some Leetcode problems

--- leetcode_tasks/leetcode.py
+++ leetcode_tasks/leetcode.py
@@ -1,16 +1,6 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# # https://leetcode.com/problems/convert-the-temperature/ |
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-
-# ~~~~~~~~~~~~~~~~~~~~~ MYSELF ~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-
-"""
-Kelvin = Celsius + 273.15
-Fahrenheit = Celsius * 1.80 + 32.00
-"""
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/concatenation-of-array/ |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 # class Solution:
-#     def convertTemperature(self, celsius: float) -> List[float]:
-#         kelvin = celsius + 273.15
-#         fahrenheit = celsius * 1.80 + 32.00
-#         return kelvin, fahrenheit
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#     def getConcatenation(self, nums: List[int]) -> List[int]:
+#         return nums + nums.copy()
