ce65c78f9ebafc8245ebc5d481527e1ada6e4b6b
l9o <ertye9020@gmail.com>
2023-12-25 19:13:46 +0500

    I solved some Leetcode tasks

--- leetcode_tasks/leetcode.py
+++ leetcode_tasks/leetcode.py
@@ -1,30 +1,9 @@
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# # https://leetcode.com/problems/longest-substring-without-repeating-characters/description/ |
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-
-# #~~~~~~~~~~~~~~~~~~~~~~ MYSELF ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/palindrome-number/description/ |
+# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # ~~~~~~~~~~~~~~~~~~ MYSELF ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         set_s = set(s)
-#         if "p" in set_s:
-#             set_s.remove("p" or "P")
-#         len_s = len(set_s)
-#         return len_s
-
-# #~~~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         n = len(s)
-#         char_index_map = {}
-#         max_length = 0
-#         start = 0
-#
-#         for end in range(n):
-#             if s[end] in char_index_map and char_index_map[s[end]] >= start:
-#                 start = char_index_map[s[end]] + 1
-#
-#             char_index_map[s[end]] = end
-#             max_length = max(max_length, end - start + 1)
-#
-#         return max_length
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#     def isPalindrome(self, x: int) -> bool:
+#         x_str = str(x)
+#         return x_str == x_str[::-1]
+# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
