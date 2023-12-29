1799fe197d88a689ca091c1705ff511d2add4a78
l9o <ertye9020@gmail.com>
2023-12-29 21:28:51 +0500

    Solved daily problem only

--- leetcode_tasks/leetcode.py
+++ leetcode_tasks/leetcode.py
@@ -1,1 +1,23 @@
-#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/zigzag-conversion/submissions/1130703655/ |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or len(s) <= numRows:
+            return s
+
+        rows = [''] * numRows
+        current_row, going_down = 0, False
+
+        for char in s:
+            rows[current_row] += char
+
+            if current_row == 0 or current_row == numRows - 1:
+                going_down = not going_down
+
+            current_row += 1 if going_down else -1
+
+        result = ''.join(rows)
+        return result
+
