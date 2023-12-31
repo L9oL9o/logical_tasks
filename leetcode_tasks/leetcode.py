1fcc95a998eb25a4363d04427df9273518477824
l9o <ertye9020@gmail.com>
2023-12-31 18:39:20 +0500

    Solved some leetcode problems and weekly contest

--- leetcode_tasks/leetcode.py
+++ leetcode_tasks/leetcode.py
@@ -1,23 +1,16 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# # https://leetcode.com/problems/zigzag-conversion/submissions/1130703655/ |
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1 or len(s) <= numRows:
-            return s
-
-        rows = [''] * numRows
-        current_row, going_down = 0, False
-
-        for char in s:
-            rows[current_row] += char
-
-            if current_row == 0 or current_row == numRows - 1:
-                going_down = not going_down
-
-            current_row += 1 if going_down else -1
-
-        result = ''.join(rows)
-        return result
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/largest-substring-between-two-equal-characters/?envType=daily-question&envId=2023-12-31 |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# class Solution:
+#     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+#         first_occurrence = {}
+#         max_length = -1
+#
+#         for i, char in enumerate(s):
+#             if char in first_occurrence:
+#                 max_length = max(max_length, i - first_occurrence[char] - 1)
+#             else:
+#                 first_occurrence[char] = i
+#
+#         return max_length
