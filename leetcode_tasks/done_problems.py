53616f08940201dc35e210eddfb70baa3254d697
l9o <ertye9020@gmail.com>
2024-01-02 20:21:07 +0500

    Solved daily problems, and some other type problems

--- leetcode_tasks/done_problems.py
+++ leetcode_tasks/done_problems.py
@@ -853,3 +853,43 @@ from typing import List
 #         if n>MAX: return MAX
 #         if n<MIN: return MIN
 #         return n
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/submissions/1133785283/?envType=daily-question&envId=2023-12-30 |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# class Solution:
+#     def makeEqual(self, words: List[str]) -> bool:
+#         char_count = Counter("".join(words))
+#         return all(count % len(words) == 0 for count in char_count.values())
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/?envType=daily-question&envId=2024-01-02 |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# from typing import List
+#
+# class Solution:
+#     def findMatrix(self, nums: List[int]) -> List[List[int]]:
+#         result = []
+#         seen = set()
+#         row_dict = defaultdict(list)
+#
+#         for num in nums:
+#             added = False
+#             for row in result:
+#                 if num not in row:
+#                     row.append(num)
+#                     seen.add(num)
+#                     added = True
+#                     break
+#
+#             if not added:
+#                 result.append([num])
+#                 seen.add(num)
+#
+#         return result
--- leetcode_tasks/leetcode.py
+++ leetcode_tasks/leetcode.py
@@ -1,52 +1,11 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# # https://leetcode.com/problems/string-to-integer-atoi/ |
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/submissions/1133785283/?envType=daily-question&envId=2023-12-30 |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 # ~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~|
 # class Solution:
-#     def myAtoi(self, s: str) -> int:
-#         # Step 1: Read in and ignore any leading whitespace.
-#         i = 0
-#         while i < len(s) and s[i].isspace():
-#             i += 1
-#
-#         # Step 2: Check if the next character is '-' or '+'
-#         if i < len(s) and (s[i] == '-' or s[i] == '+'):
-#             sign = -1 if s[i] == '-' else 1
-#             i += 1
-#         else:
-#             sign = 1
-#
-#         # Step 3: Read in the characters until the next non-digit character or end of input
-#         result = 0
-#         while i < len(s) and s[i].isdigit():
-#             digit = int(s[i])
-#             # Step 4: Convert digits into an integer
-#             result = result * 10 + digit
-#             i += 1
-#
-#         # Step 5: Change the sign if necessary
-#         result *= sign
-#
-#         # Step 6: Clamp the result to the 32-bit signed integer range
-#         INT_MAX = 2**31 - 1
-#         INT_MIN = -2**31
-#         result = max(min(result, INT_MAX), INT_MIN)
-#
-#         return result
-
-# ~~~~~~~~~~~~~~~~~~~~~~~ LEETCODE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# import re
-#
-# class Solution:
-#     def myAtoi(self, s: str) -> int:
-#         s = s.strip()
-#         s = re.findall("^[+\-]?\d+",s)
-#         if not s: return 0
-#         n = int("".join(s))
-#         MAX = pow(2,31)-1
-#         MIN = -pow(2,31)
-#         if n>MAX: return MAX
-#         if n<MIN: return MIN
-#         return n
-
-
+#     def makeEqual(self, words: List[str]) -> bool:
+#         char_count = Counter("".join(words))
+#         return all(count % len(words) == 0 for count in char_count.values())
