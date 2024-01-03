988845818cab671f62605c8b789eef4985c97352
l9o <ertye9020@gmail.com>
2024-01-03 21:13:36 +0500

    Solved one more hard problem

--- leetcode_tasks/hard_problems.py
+++ leetcode_tasks/hard_problems.py
@@ -99,3 +99,39 @@
 #                 min_heap.put((node.next.val, index, node.next))
 #
 #         return dummy.next
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~ HARD ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/substring-with-concatenation-of-all-words/ |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# class Solution:
+#     def findSubstring(self, s: str, words: List[str]) -> List[int]:
+#         if not s or not words:
+#             return []
+#
+#         word_len = len(words[0])
+#         word_count = len(words)
+#         total_len = word_len * word_count
+#         word_freq = Counter(words)
+#
+#         result = []
+#
+#         for i in range(word_len):
+#             left, right = i, i
+#             current_window = Counter()
+#
+#             while right + word_len <= len(s):
+#                 current_word = s[right:right + word_len]
+#                 right += word_len
+#                 current_window[current_word] += 1
+#
+#                 while current_window[current_word] > word_freq[current_word]:
+#                     current_window[s[left:left + word_len]] -= 1
+#                     left += word_len
+#
+#                 if right - left == total_len:
+#                     result.append(left)
+#
+#         return result
--- leetcode_tasks/leetcode.py
+++ leetcode_tasks/leetcode.py
@@ -1,37 +1,33 @@
-# ~~~~~~~~~~~~~~~~~~~~ HARD ~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# # https://leetcode.com/problems/merge-k-sorted-lists |
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# ~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# from queue import PriorityQueue
-# from typing import List, Optional
-#
-# # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-#
+# ~~~~~~~~~~~~~~~~~~~~~~~ HARD ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/substring-with-concatenation-of-all-words/ |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 # class Solution:
-#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-#         # Create a priority queue (min-heap) to keep track of the current minimum node
-#         min_heap = PriorityQueue()
+#     def findSubstring(self, s: str, words: List[str]) -> List[int]:
+#         if not s or not words:
+#             return []
 #
-#         # Add the first node from each list to the min-heap
-#         for i, lst in enumerate(lists):
-#             if lst:
-#                 min_heap.put((lst.val, i, lst))
+#         word_len = len(words[0])
+#         word_count = len(words)
+#         total_len = word_len * word_count
+#         word_freq = Counter(words)
 #
-#         # Dummy node to simplify the code
-#         dummy = ListNode()
-#         current = dummy
+#         result = []
 #
-#         while not min_heap.empty():
-#             val, index, node = min_heap.get()
-#             current.next = node
-#             current = current.next
+#         for i in range(word_len):
+#             left, right = i, i
+#             current_window = Counter()
 #
-#             # Move to the next node in the list
-#             if node.next:
-#                 min_heap.put((node.next.val, index, node.next))
+#             while right + word_len <= len(s):
+#                 current_word = s[right:right + word_len]
+#                 right += word_len
+#                 current_window[current_word] += 1
 #
-#         return dummy.next
+#                 while current_window[current_word] > word_freq[current_word]:
+#                     current_window[s[left:left + word_len]] -= 1
+#                     left += word_len
+#
+#                 if right - left == total_len:
+#                     result.append(left)
+#
+#         return result
