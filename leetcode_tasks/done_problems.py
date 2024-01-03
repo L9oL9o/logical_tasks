61cf6b5ca3e9664a889a7feff0640c714c4a7ecf
l9o <ertye9020@gmail.com>
2024-01-03 20:04:16 +0500

    Solved some daily problems, and I don't write solved problems in done_problems.txt anymore.

--- leetcode_tasks/done_problems.py
+++ leetcode_tasks/done_problems.py
@@ -969,3 +969,58 @@ from typing import List
 #             if curr:
 #                 prev = curr
 #         return res
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/valid-parentheses/ |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~~ MYSELF ~~~~~~~~~~~~~~~~~~~~~~|
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         brackets1 = "()"
+#         brackets2 = "[]"
+#         brackets3 = "{}"
+#         check_bracket = ""
+#         for i in range(len(s)):
+#             for j in range(len(s) + 1):
+#                 if s[i] and s[j] == brackets1:
+#                     check_bracket += s[i], s[j]
+#                 elif s[i] and s[j] == brackets2:
+#                     check_bracket += s[i], s[j]
+#                 elif s[i] and s[j] == brackets3:
+#                     check_bracket += s[i], s[j]
+#         return check_bracket
+
+# ~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         bracket_pairs = {')': '(', '}': '{', ']': '['}
+#         for char in s:
+#             if char in bracket_pairs.values():
+#                 stack.append(char)
+#             elif char in bracket_pairs.keys():
+#                 if not stack or bracket_pairs[char] != stack.pop():
+#                     return False
+#             else:
+#                 # If the character is not an open or close bracket, ignore it
+#                 continue
+#         # The string is valid if the stack is empty after processing all characters
+#         return not stack
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~ LEETCODE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         check = {
+#             '(': ')',
+#             '{': '}',
+#             '[': ']'
+#         }
+#         stack = []
+#         for c in s:
+#             if stack and stack[-1] in check and check[stack[-1]] == c:
+#                 stack.pop()
+#             else:
+#                 stack.append(c)
+#         return not stack
--- leetcode_tasks/leetcode.py
+++ leetcode_tasks/leetcode.py
@@ -1,15 +1,52 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# # https://leetcode.com/problems/number-of-laser-beams-in-a-bank/?envType=daily-question&envId=2024-01-03 |
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ YOUTUBE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/valid-parentheses/ |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~~ MYSELF ~~~~~~~~~~~~~~~~~~~~~~|
 # class Solution:
-#     def numberOfBeam(self, bank: List[str]) -> int:
-#         prev = bank[0].count("1")
-#         res = 0
-#
-#         for i in range(1, len(bank)):
-#             curr = bank[i].count("1")
-#             res += (prev * curr)
-#             if curr:
-#                 prev = curr
-#         return res
+#     def isValid(self, s: str) -> bool:
+#         brackets1 = "()"
+#         brackets2 = "[]"
+#         brackets3 = "{}"
+#         check_bracket = ""
+#         for i in range(len(s)):
+#             for j in range(len(s) + 1):
+#                 if s[i] and s[j] == brackets1:
+#                     check_bracket += s[i], s[j]
+#                 elif s[i] and s[j] == brackets2:
+#                     check_bracket += s[i], s[j]
+#                 elif s[i] and s[j] == brackets3:
+#                     check_bracket += s[i], s[j]
+#         return check_bracket
+
+# ~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         bracket_pairs = {')': '(', '}': '{', ']': '['}
+#         for char in s:
+#             if char in bracket_pairs.values():
+#                 stack.append(char)
+#             elif char in bracket_pairs.keys():
+#                 if not stack or bracket_pairs[char] != stack.pop():
+#                     return False
+#             else:
+#                 # If the character is not an open or close bracket, ignore it
+#                 continue
+#         # The string is valid if the stack is empty after processing all characters
+#         return not stack
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~ LEETCODE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         check = {
+#             '(': ')',
+#             '{': '}',
+#             '[': ']'
+#         }
+#         stack = []
+#         for c in s:
+#             if stack and stack[-1] in check and check[stack[-1]] == c:
+#                 stack.pop()
+#             else:
+#                 stack.append(c)
+#         return not stack
