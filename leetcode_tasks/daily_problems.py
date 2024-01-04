4ca071752eb3be9da1c65d754e643d649a267cf3
l9o <ertye9020@gmail.com>
2024-01-04 19:58:20 +0500

    Updated daily_problems.py, added daily problems from 1 January 2024, also planning to devide by parts every month daily problems in seperate files

--- leetcode_tasks/daily_problems.py
+++ leetcode_tasks/daily_problems.py
@@ -1,4 +1,90 @@
-# 3 January
+# 21 DECEMBER 2023
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/description/?envType=daily-question&envId=2023-12-21 |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# class Solution:
+#     def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+#         points.sort(key=lambda x: x[0])  # Sort points based on x-coordinate
+#
+#         max_width = 0
+#         for i in range(1, len(points)):
+#             max_width = max(max_width, points[i][0] - points[i - 1][0])
+#
+#         return max_width
+
+
+# 1 JANUARY 2024
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/assign-cookies/?envType=daily-question&envId=2024-01-01 |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# class Solution:
+#     def findContentChildren(self, g: List[int], s: List[int]) -> int:
+#         # Sort the greed factors and cookie sizes in ascending order
+#         g.sort()
+#         s.sort()
+#
+#         content_children = 0
+#         i = 0  # Index for greed factors
+#         j = 0  # Index for cookie sizes
+#
+#         while i < len(g) and j < len(s):
+#             if s[j] >= g[i]:
+#                 # If the current cookie size is sufficient for the current child
+#                 content_children += 1
+#                 i += 1  # Move to the next child
+#             j += 1  # Move to the next cookie
+#
+#         return content_children
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LEETCODE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# class Solution:
+#     def findContentChildren(self, g: List[int], s: List[int]) -> int:
+#         g.sort()
+#         s.sort()
+#         l = len(g) - 1
+#         m = len(s) - 1
+#         count = 0
+#         while l>-1 and m>-1:
+#             if g[l]<=s[m]:
+#                 l-=1
+#                 m-=1
+#                 count+=1
+#             else:
+#                 l-=1
+#         return count
+
+
+# 2 JANUARY 2024
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/?envType=daily-question&envId=2024-01-02 |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# from typing import List
+#
+# class Solution:
+#     def findMatrix(self, nums: List[int]) -> List[List[int]]:
+#         result = []
+#         seen = set()
+#         row_dict = defaultdict(list)
+#
+#         for num in nums:
+#             added = False
+#             for row in result:
+#                 if num not in row:
+#                     row.append(num)
+#                     seen.add(num)
+#                     added = True
+#                     break
+#
+#             if not added:
+#                 result.append([num])
+#                 seen.add(num)
+#
+#         return result
+
+
+
+# 3 JANUARY 2024
 # ~~~~~~~~~~~~~ 2125. Number of Laser Beams in a Bank ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 # # https://leetcode.com/problems/number-of-laser-beams-in-a-bank/?envType=daily-question&envId=2024-01-03 |
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
@@ -17,6 +103,7 @@
 
 
 
+# 4 JANUARY 2024
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 # # https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/description/?envType=daily-question&envId=2024-01-04 |
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
