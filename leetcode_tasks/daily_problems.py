79b1f51e9612975a95218ae91213c39bcfbfdea9
l9o <ertye9020@gmail.com>
2024-01-07 23:20:13 +0500

    I solved some problems in leetcode myself

--- leetcode_tasks/daily_problems.py
+++ leetcode_tasks/daily_problems.py
@@ -205,3 +205,21 @@
 #                 total_count += prev_count
 #
 #         return total_count
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # https://leetcode.com/problems/length-of-last-word |
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# ~~~~~~~~~~~~~~~~~~~~ MYSELF ~~~~~~~~~~~~~~~~~~~~~~~~|
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         words = s.split()
+#         if words:
+#             s = words[-1]
+#             return len(s)
+
+# ~~~~~~~~~~~~~~ LEETCODE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         return len(s.strip().split(' ')[-1])
