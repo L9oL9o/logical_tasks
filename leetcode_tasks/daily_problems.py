4bea718cd05d627e42655f06fc9013d2a70c4319
l9o <ertye9020@gmail.com>
2024-02-01 01:16:06 +0500

    Solved daily question. Got January BADGE git add . !

--- leetcode_tasks/daily_problems.py
+++ leetcode_tasks/daily_problems.py
@@ -879,3 +879,23 @@
 #     print("0",file=f)
 #     print("-2147483648",file=f)
 # exit(0)
+
+# 31 JANUARY 2024
+# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# https://leetcode.com/problems/daily-temperatures/description/?envType=daily-question&envId=2024-01-31 |
+# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# # ~~~~~~~~~~~~~~~~~~~~~~~~~~ GPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+# def dailyTemperatures(temperatures: List[int]) -> List[int]:
+#
+#     output = [0]*len(temperatures)
+#     stack = []
+#     for cindex,ctemp  in enumerate(temperatures):
+#         while stack and stack[-1][0] < ctemp:#cause it is monotonically decreasing
+#                 t,i = stack.pop()
+#                 output[i] = cindex-i
+#         stack.append((ctemp, cindex))
+#     return output
+# with open('user.out', 'w') as f:
+#     for case in map(loads, stdin):
+#         f.write(f"[{','.join([str(x) for x in dailyTemperatures(case)])}]\n")
+# exit()
