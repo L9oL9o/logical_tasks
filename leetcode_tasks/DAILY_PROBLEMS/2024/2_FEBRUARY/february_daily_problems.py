f469e177b202c4c2137ca89a4f13b3aad15e6d0a
l9o <ertye9020@gmail.com>
2024-02-07 11:13:31 +0500

    Solved some daily problems

--- leetcode_tasks/DAILY_PROBLEMS/2024/2_FEBRUARY/february_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/2_FEBRUARY/february_daily_problems.py
@@ -32,14 +32,104 @@
 
 
 # 3 FEBRUARY 2024
+# https://leetcode.com/problems/partition-array-for-maximum-sum/description/?envType=daily-question&envId=2024-02-03
+
+# class Solution:
+#     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+#         n = len(arr)
+#         dp = [0]*(n+1)
+#         for i in range(n):
+#             curMax = curSum = 0
+#             for j in range(i, max(-1, i-k), -1):
+#                 if curMax < arr[j]:
+#                     curMax = arr[j]
+#                 cur = curMax*(i-j+1)+dp[j]
+#                 if curSum < cur:
+#                     curSum = cur
+#             dp[i+1] = curSum
+#         return dp[-1]
+#
+
 
 # 4 FEBRUARY 2024
+# https://leetcode.com/problems/minimum-window-substring/description/?envType=daily-question&envId=2024-02-04
+
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         if len(s) < len(t):
+#             return ""
+#         needstr = collections.defaultdict(int)
+#         for ch in t:
+#             needstr[ch] += 1
+#         needcnt = len(t)
+#         res = (0, float('inf'))
+#         start = 0
+#         for end, ch in enumerate(s):
+#             if needstr[ch] > 0:
+#                 needcnt -= 1
+#             needstr[ch] -= 1
+#             if needcnt == 0:
+#                 while True:
+#                     tmp = s[start]
+#                     if needstr[tmp] == 0:
+#                         break
+#                     needstr[tmp] += 1
+#                     start += 1
+#                 if end - start < res[1] - res[0]:
+#                     res = (start, end)
+#                 needstr[s[start]] += 1
+#                 needcnt += 1
+#                 start += 1
+#         return '' if res[1] > len(s) else s[res[0]:res[1]+1]
+
 
 # 5 FEBRUARY 2024
+# https://leetcode.com/problems/first-unique-character-in-a-string/description/?envType=daily-question&envId=2024-02-05
+
+# class Solution:
+#     def firstUniqChar(self, s: str) -> int:
+#         char_count = {}
+#
+#         # Count the frequency of each character in the string
+#         for char in s:
+#             char_count[char] = char_count.get(char, 0) + 1
+#
+#         # Iterate through the string to find the first unique character
+#         for i in range(len(s)):
+#             if char_count[s[i]] == 1:
+#                 return i
+#
+#         # If no unique character is found, return -1
+#         return -1
+
 
 # 6 FEBRUARY 2024
+# https://leetcode.com/problems/group-anagrams/description/?envType=daily-question&envId=2024-02-06
+
+# f = open('user.out', 'w')
+#
+# for strs in map(loads, stdin):
+#     w = defaultdict(list)
+#     for s in strs:
+#         w[''.join(sorted(s))].append(s)
+#
+#     print(str(w.values())[12:-1].replace("'", '"').replace(" ", ""), file=f)
+# exit()
+
 
 # 7 FEBRUARY 2024
+# https://leetcode.com/problems/sort-characters-by-frequency/description/?envType=daily-question&envId=2024-02-07
+
+# class Solution:
+#     def frequencySort(self, s: str) -> str:
+#         k = Counter(s)
+#         k4 = ''
+#         # Sort characters by frequency and then by dictionary order
+#         sorted_chars = sorted(k, key=lambda x: (-k[x], x))
+#         for char in sorted_chars:
+#             k4 += char * k[char]
+#         return k4
+
 
 # 8 FEBRUARY 2024
 
