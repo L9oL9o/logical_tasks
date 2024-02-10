711470ed8a1f8dd7c0813ad515f7c7ff1316565d
l9o <ertye9020@gmail.com>
2024-02-10 14:03:52 +0500

    Solved daily problem 10-feb

--- leetcode_tasks/DAILY_PROBLEMS/2024/2_FEBRUARY/february_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/2_FEBRUARY/february_daily_problems.py
@@ -148,7 +148,6 @@
 
 
 # 9 FEBRUARY 2024
-
 # https://leetcode.com/problems/largest-divisible-subset/description/?envType=daily-question&envId=2024-02-09
 
 # class Solution:
@@ -175,6 +174,20 @@
 
 
 # 10 FEBRUARY 2024
+# https://leetcode.com/problems/palindromic-substrings/description/?envType=daily-question&envId=2024-02-10
+
+# class Solution:
+#     def countSubstrings(self, S: str) -> int:
+#         ans, n, i = 0, len(S), 0
+#         while (i < n):
+#             j, k = i - 1, i
+#             while k < n - 1 and S[k] == S[k+1]: k += 1
+#             ans += (k - j) * (k - j + 1) // 2
+#             i, k = k + 1, k + 1
+#             while ~j and k < n and S[k] == S[j]:
+#                 j, k, ans = j - 1, k + 1, ans + 1
+#         return ans
+
 
 # 11 FEBRUARY 2024
 
