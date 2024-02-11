5f8f87731ad84b432e24863129bac079b818f308
l9o <ertye9020@gmail.com>
2024-02-11 19:04:45 +0500

    Solved daily problem. 11-feb

--- leetcode_tasks/DAILY_PROBLEMS/2024/2_FEBRUARY/february_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/2_FEBRUARY/february_daily_problems.py
@@ -190,6 +190,30 @@
 
 
 # 11 FEBRUARY 2024
+# https://leetcode.com/problems/cherry-pickup-ii/description/?envType=daily-question&envId=2024-02-11
+
+# class Solution:
+#     def cherryPickup(self, grid: List[List[int]]) -> int:
+#         m, n = len(grid), len(grid[0])
+#         if n == 2:
+#             return sum(sum(g) for g in grid)
+#         elif n == 3:
+#             return sum(sum(g) - min(g) for g in grid)
+#
+#         prev, dirs = [[0] * n for _ in range(n)], {(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0),
+#                                                    (1, 1)}
+#         for i in range(m - 1, -1, -1):
+#             cur = [[0] * n for _ in range(n)]
+#             for j in range(min(i + 1, n)):
+#                 for k in range(max(j, n - i - 1), n):
+#                     for dir in dirs:
+#                         s, t = j + dir[0], k + dir[1]
+#                         if 0 <= s <= t < n and cur[j][k] < prev[s][t]:
+#                             cur[j][k] = prev[s][t]
+#                     cur[j][k] += grid[i][j] + (0 if j == k else grid[i][k])
+#             prev = cur
+#         return prev[0][-1]
+
 
 # 12 FEBRUARY 2024
 
