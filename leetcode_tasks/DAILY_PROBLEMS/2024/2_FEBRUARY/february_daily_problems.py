b4adb0ce4041ef2e9487f9673b772987f4cedb34
l9o <ertye9020@gmail.com>
2024-02-25 12:15:09 +0500

    Solved daily problem Feb-25

--- leetcode_tasks/DAILY_PROBLEMS/2024/2_FEBRUARY/february_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/2_FEBRUARY/february_daily_problems.py
@@ -461,6 +461,24 @@
 
 
 # 25 FEBRUARY 2024
+# https://leetcode.com/problems/greatest-common-divisor-traversal/description/?envType=daily-question&envId=2024-02-25
+
+# class Solution:
+#     def canTraverseAllPairs(self, nums: List[int]) -> bool:
+#         if len(nums) == 1: return True
+#         nums = set(nums)
+#         if 1 in nums: return False
+#         if len(nums) == 1: return True
+#         nums = sorted(nums, reverse=True)
+#         for i in range(len(nums) - 1):
+#             for j in range(i + 1, len(nums)):
+#                 if gcd(nums[i], nums[j]) - 1:
+#                     nums[j] *= nums[i]
+#                     break
+#             else:
+#                 return False
+#         return True
+
 
 # 26 FEBRUARY 2024
 
