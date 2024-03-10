517b399e3cad60fe151e4c846fbc80c1cd981214
l9o <ertye9020@gmail.com>
2024-03-10 23:50:53 +0500

    Solved daily problem March-10

--- leetcode_tasks/DAILY_PROBLEMS/2024/3_MARCH/march_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/3_MARCH/march_daily_problems.py
@@ -138,6 +138,14 @@ from builtins import list
 #         return -1
 
 # 10 MARCH
+# https://leetcode.com/problems/intersection-of-two-arrays/description/?envType=daily-question&envId=2024-03-10
+
+# class Solution:
+#     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         set1 = set(nums1)
+#         set2 = set(nums2)
+#         return list(set1.intersection(set2))
+
 
 # 11 MARCH
 
