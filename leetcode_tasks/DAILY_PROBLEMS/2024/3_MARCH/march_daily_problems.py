f94ce64f11f21a42edb4afb83092c15a658462cc
l9o <ertye9020@gmail.com>
2024-03-16 00:04:08 +0500

    Solved daily problem March-15

--- leetcode_tasks/DAILY_PROBLEMS/2024/3_MARCH/march_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/3_MARCH/march_daily_problems.py
@@ -212,6 +212,22 @@ from builtins import list
 
 
 # 15 MARCH
+# https://leetcode.com/problems/product-of-array-except-self/?envType=daily-question&envId=2024-03-15
+
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         n = len(nums)
+#         prefix_product = 1
+#         postfix_product = 1
+#         result = [0]*n
+#         for i in range(n):
+#             result[i] = prefix_product
+#             prefix_product *= nums[i]
+#         for i in range(n-1,-1,-1):
+#             result[i] *= postfix_product
+#             postfix_product *= nums[i]
+#         return result
+
 
 # 16 MARCH
 
