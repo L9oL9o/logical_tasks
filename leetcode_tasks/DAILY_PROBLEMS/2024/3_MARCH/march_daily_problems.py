05d73bea62e63dd343fbd8a273994c407e2c266f
l9o <ertye9020@gmail.com>
2024-03-17 21:12:53 +0500

    Solved daily problem March-17

--- leetcode_tasks/DAILY_PROBLEMS/2024/3_MARCH/march_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/3_MARCH/march_daily_problems.py
@@ -251,6 +251,28 @@ from builtins import list
 
 
 # 17 MARCH
+# https://leetcode.com/problems/insert-interval/description/?envType=daily-question&envId=2024-03-17
+
+# class Solution:
+#     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+#         merged = []
+#         i = 0
+#
+#         while i < len(intervals) and intervals[i][1] < newInterval[0]:
+#             merged.append(intervals[i])
+#             i += 1
+#
+#         while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+#             newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+#             i += 1
+#         merged.append(newInterval)
+#
+#         while i < len(intervals):
+#             merged.append(intervals[i])
+#             i += 1
+#
+#         return merged
+
 
 # 18 MARCH
 
