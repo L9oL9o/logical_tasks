2691dfa0f0fbc174d4020c665614caa0a4b11ccd
l9o <ertye9020@gmail.com>
2024-03-19 21:20:55 +0500

    Solved daily problem March-19

--- leetcode_tasks/DAILY_PROBLEMS/2024/3_MARCH/march_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/3_MARCH/march_daily_problems.py
@@ -291,6 +291,20 @@ from builtins import list
 
 
 # 19 MARCH
+# https://leetcode.com/problems/task-scheduler/description/?envType=daily-question&envId=2024-03-19
+
+# class Solution:
+#     def leastInterval(self, tasks: List[str], n: int) -> int:
+#         freq = [0] * 26
+#         for task in tasks:
+#             freq[ord(task) - ord('A')] += 1
+#         freq.sort()
+#         chunk = freq[25] - 1
+#         idle = chunk * n
+#         for i in range(24, -1, -1):
+#             idle -= min(chunk, freq[i])
+#         return len(tasks) + idle if idle >= 0 else len(tasks)
+
 
 # 20 MARCH
 
