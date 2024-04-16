60d40f7c71b80b9643ed0d02e4a236607261c500
L9oL9o <ertye9020@gmail.com>
2024-04-16 23:09:37 +0500

    Solved daily problem APRIL-16

--- leetcode_tasks/DAILY_PROBLEMS/2024/4_APRIL/april_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/4_APRIL/april_daily_problems.py
@@ -304,7 +304,37 @@
 
 
 # 16 APRIL
+# https://leetcode.com/problems/add-one-row-to-tree/description/?envType=daily-question&envId=2024-04-16
+
+# class Solution:
+#     def add(self, root, val, depth, curr):
+#         if not root:
+#             return None
 #
+#         if curr == depth - 1:
+#             lTemp = root.left
+#             rTemp = root.right
+#
+#             root.left = TreeNode(val)
+#             root.right = TreeNode(val)
+#             root.left.left = lTemp
+#             root.right.right = rTemp
+#
+#             return root
+#
+#         root.left = self.add(root.left, val, depth, curr + 1)
+#         root.right = self.add(root.right, val, depth, curr + 1)
+#
+#         return root
+#
+#     def addOneRow(self, root, val, depth):
+#         if depth == 1:
+#             newRoot = TreeNode(val)
+#             newRoot.left = root
+#             return newRoot
+#
+#         return self.add(root, val, depth, 1)
+
 
 # 17 APRIL
 #
