8c317728a7913d8c176eb8fea5414e666ffbb151
L9oL9o <ertye9020@gmail.com>
2024-04-18 11:10:38 +0500

    Solved daily problem APRIL-18

--- leetcode_tasks/DAILY_PROBLEMS/2024/4_APRIL/april_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/4_APRIL/april_daily_problems.py
@@ -371,7 +371,24 @@
 
 
 # 18 APRIL
+# https://leetcode.com/problems/island-perimeter/description/?envType=daily-question&envId=2024-04-18
+
+# class Solution:
+#     def islandPerimeter(self, grid: List[List[int]]) -> int:
+#         perimeter = 0
+#         rows, cols = len(grid), len(grid[0])
 #
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if grid[r][c] == 1:
+#                     perimeter += 4
+#                     if r > 0 and grid[r - 1][c] == 1:
+#                         perimeter -= 2
+#                     if c > 0 and grid[r][c - 1] == 1:
+#                         perimeter -= 2
+#
+#         return perimeter
+
 
 # 19 APRIL
 #
