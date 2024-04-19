997f51fd975558b05e8f8f3c3309ad7428f1a4ba
L9oL9o <ertye9020@gmail.com>
2024-04-19 14:40:02 +0500

    Solved daily problem APRIL-19

--- leetcode_tasks/DAILY_PROBLEMS/2024/4_APRIL/april_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/4_APRIL/april_daily_problems.py
@@ -391,7 +391,31 @@
 
 
 # 19 APRIL
+# https://leetcode.com/problems/number-of-islands/description/?envType=daily-question&envId=2024-04-19
+
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         if not grid:
+#             return 0
 #
+#         def dfs(i, j):
+#             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+#                 return
+#             grid[i][j] = '0'  # mark as visited
+#             dfs(i + 1, j)
+#             dfs(i - 1, j)
+#             dfs(i, j + 1)
+#             dfs(i, j - 1)
+#
+#         num_islands = 0
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 if grid[i][j] == '1':
+#                     num_islands += 1
+#                     dfs(i, j)
+#
+#         return num_islands
+
 
 # 20 APRIL
 #
