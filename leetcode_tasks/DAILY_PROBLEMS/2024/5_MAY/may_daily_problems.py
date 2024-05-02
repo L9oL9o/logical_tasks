28cf75633e283dee9e77cc04d31f632486f90740
L9oL9o <ertye9020@gmail.com>
2024-05-02 23:28:24 +0500

    Solved daily problem MAY-2

--- leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
@@ -1,5 +1,4 @@
 # 1 MAY
-
 # https://leetcode.com/problems/reverse-prefix-of-word/description/?envType=daily-question&envId=2024-05-01
 
 # class Solution:
@@ -8,3 +7,132 @@
 #         if j != -1:
 #             return word[:j+1][::-1] + word[j+1:]
 #         return word
+
+
+# 2 MAY
+# https://leetcode.com/problems/largest-positive-integer-that-exists-with-its-negative/description/?envType=daily-question&envId=2024-05-02
+
+# class Solution:
+#     def findMaxK(self, nums: List[int]) -> int:
+#         nums.sort()
+#         n = len(nums)
+#         for i in range(n-1, -1, -1):
+#             if nums[i] > 0 and -nums[i] in nums:
+#                 return nums[i]
+#         return -1  # If no such pair found
+
+
+# 3 MAY
+#
+
+
+# 4 MAY
+#
+
+
+# 5 MAY
+#
+
+
+# 6 MAY
+#
+
+
+# 7 MAY
+#
+
+
+# 8 MAY
+#
+
+
+# 9 MAY
+#
+
+
+# 10 MAY
+#
+
+
+# 11 MAY
+#
+
+
+# 12 MAY
+#
+
+
+# 13 MAY
+#
+
+
+# 14 MAY
+#
+
+
+# 15 MAY
+#
+
+
+# 16 MAY
+#
+
+
+# 17 MAY
+#
+
+
+# 18 MAY
+#
+
+
+# 19 MAY
+#
+
+
+# 20 MAY
+#
+
+
+# 21 MAY
+#
+
+
+# 22 MAY
+#
+
+
+# 23 MAY
+#
+
+
+# 24 MAY
+#
+
+
+# 25 MAY
+#
+
+
+# 26 MAY
+#
+
+
+# 27 MAY
+#
+
+
+# 28 MAY
+#
+
+
+# 29 MAY
+#
+
+
+# 30 MAY
+#
+
+
+# 31 MAY
+#
