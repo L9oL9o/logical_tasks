bbd3f40ad12b41f3d4ad3251fbbcf3b4e33d3eed
L9oL9o <ertye9020@gmail.com>
2024-05-09 23:54:30 +0500

    Solved daily problems MAY 8-9

--- leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
@@ -150,11 +150,53 @@
 
 
 # 8 MAY
+# https://leetcode.com/problems/relative-ranks/description/?envType=daily-question&envId=2024-05-08
+
+# class Solution:
+#     def findRelativeRanks(self, score: List[int]) -> List[str]:
+#         N = len(score)
 #
+#         # Create a heap of pairs (score, index)
+#         heap = []
+#         for index, score in enumerate(score):
+#             heapq.heappush(heap, (-score, index))
+#
+#         # Assign ranks to athletes
+#         rank = [0] * N
+#         place = 1
+#         while heap:
+#             original_index = heapq.heappop(heap)[1]
+#             if place == 1:
+#                 rank[original_index] = "Gold Medal"
+#             elif place == 2:
+#                 rank[original_index] = "Silver Medal"
+#             elif place == 3:
+#                 rank[original_index] = "Bronze Medal"
+#             else:
+#                 rank[original_index] = str(place)
+#             place += 1
+#
+#         return rank
+
+
 
 
 # 9 MAY
+# https://leetcode.com/problems/maximize-happiness-of-selected-children/description/?envType=daily-question&envId=2024-05-09
+
+# class Solution:
+#     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+#         happiness.sort(reverse=True)
+#         i = 0
+#         res = 0
 #
+#         while k > 0:
+#             happiness[i] = max(happiness[i] - i, 0)
+#             res += happiness[i]
+#             i += 1
+#             k -= 1
+#
+#         return res
 
 
 # 10 MAY
