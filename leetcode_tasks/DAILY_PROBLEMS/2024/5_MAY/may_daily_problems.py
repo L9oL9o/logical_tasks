b4c4e90fbcfc85948f1785bc2f66df9db715d333
L9oL9o <ertye9020@gmail.com>
2024-05-20 19:30:24 +0500

    Solved daily problems MAY 19-20

--- leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
@@ -307,11 +307,46 @@
 
 
 # 19 MAY
+# https://leetcode.com/problems/find-the-maximum-sum-of-node-values/?envType=daily-question&envId=2024-05-19
+
+# class Solution:
+#     def maximumValueSum(self, nums: list[int], k: int, edges: list[list[int]]) -> int:
+#         n: int = len(nums)
+#         temp: list[list[int]] = [[-1 for _ in range(2)] for _ in range(n)]  # temp[current_index(node)][is_even]
 #
+#         def calculate_max(cur_ind, is_even) -> int:  # cur_ind -> cur_index of the tree and is_even represents whether we have already changed (XOR) even or odd number of nodes
+#             if cur_ind == n:  # if we go to node which doesn't exist
+#                 return 0 if is_even else -float("inf")
+#             if temp[cur_ind][is_even] != -1:  # if we've already encountered this state
+#                 return temp[cur_ind][is_even]
+#
+#             # checking all possible variants (no XOR or XOR)
+#             no_xor = nums[cur_ind] + calculate_max(cur_ind + 1, is_even)  # we don't change the number of XOR nodes
+#             with_xor = (nums[cur_ind] ^ k) + calculate_max(cur_ind + 1, not is_even)  # we added 1 XORed node
+#
+#             mx_possible = max(no_xor, with_xor)
+#             temp[cur_ind][is_even] = mx_possible
+#             return mx_possible
+#
+#         return calculate_max(0, 1)  # is_even == 1 because we have XORed 0 nodes which is even
 
 
 # 20 MAY
-#
+# https://leetcode.com/problems/sum-of-all-subset-xor-totals/description/?envType=daily-question&envId=2024-05-20
+
+# class Solution:
+#     def subsetXORSum(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         total_sum = 0
+#         # Iterate through all possible subsets
+#         for i in range(1 << n):
+#             subset_xor = 0
+#             for j in range(n):
+#                 # Check if the j-th element is in the i-th subset
+#                 if i & (1 << j):
+#                     subset_xor ^= nums[j]
+#             total_sum += subset_xor
+#         return total_sum
 
 
 # 21 MAY
