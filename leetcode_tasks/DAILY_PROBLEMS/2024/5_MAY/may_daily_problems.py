7fc62c7e93cad077cd34d2a51d993b208b936b9e
L9oL9o <ertye9020@gmail.com>
2024-05-21 06:58:53 +0500

    Solved daily problem MAY-21

--- leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
@@ -350,7 +350,24 @@
 
 
 # 21 MAY
+# https://leetcode.com/problems/subsets/description/?envType=daily-question&envId=2024-05-21
+
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+#         op = []
+#         self.solve(nums, 0, op, res)
+#         return res
 #
+#     def solve(self, nums: List[int], start: int, op: List[int], res: List[List[int]]):
+#         if start == len(nums):
+#             res.append(op.copy())
+#             return
+#
+#         self.solve(nums, start + 1, op, res)
+#         op.append(nums[start])
+#         self.solve(nums, start + 1, op, res)
+#         op.pop()
 
 
 # 22 MAY
