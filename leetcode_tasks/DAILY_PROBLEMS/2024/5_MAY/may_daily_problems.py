c7d3be3c35b879f20a516aed47d5f32bbe182b26
L9oL9o <ertye9020@gmail.com>
2024-05-22 22:03:40 +0500

    Solved daily problem MAY-22

--- leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
@@ -371,7 +371,24 @@
 
 
 # 22 MAY
+# https://leetcode.com/problems/palindrome-partitioning/?envType=daily-question&envId=2024-05-22
+
+# class Solution:
+#     def partition(self, s: str) -> List[List[str]]:
+#         def is_palindrome(sub):
+#             return sub == sub[::-1]
 #
+#         def backtrack(start, path):
+#             if start == len(s):
+#                 result.append(path[:])
+#                 return
+#             for end in range(start + 1, len(s) + 1):
+#                 if is_palindrome(s[start:end]):
+#                     backtrack(end, path + [s[start:end]])
+#
+#         result = []
+#         backtrack(0, [])
+#         return result
 
 
 # 23 MAY
