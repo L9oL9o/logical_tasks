c68796791f8f449c6f8b95ad9b639cdfc8148dcf
L9oL9o <ertye9020@gmail.com>
2024-05-24 16:40:58 +0500

    Solved daily problem MAY-24

--- leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
@@ -429,7 +429,40 @@
 
 
 # 24 MAY
+# https://leetcode.com/problems/maximum-score-words-formed-by-letters/description/?envType=daily-question&envId=2024-05-24
+
+# class Solution:
+#     def maxScoreWords(
+#         self, words: List[str], letters: List[str], score: List[int]
+#     ) -> int:
+#         lettersCounter = Counter(letters)
+#         totalScore = 0
 #
+#         def explore(index, letterCounter, currScore):
+#             nonlocal totalScore
+#
+#             totalScore = max(totalScore, currScore)
+#             if index == len(words):
+#                 return
+#
+#             for i in range(index, len(words)):
+#                 tmpCounter = copy.deepcopy(letterCounter)
+#                 word = words[i]
+#                 wordScore = 0
+#                 isValid = True
+#
+#                 for ch in word:
+#                     if ch in tmpCounter and tmpCounter[ch] > 0:
+#                         tmpCounter[ch] -= 1
+#                         wordScore += score[ord(ch) - ord("a")]
+#                     else:
+#                         isValid = False
+#                         break
+#                 if isValid:
+#                     explore(i + 1, tmpCounter, currScore + wordScore)
+#
+#         explore(0, lettersCounter, 0)
+#         return totalScore
 
 
 # 25 MAY
