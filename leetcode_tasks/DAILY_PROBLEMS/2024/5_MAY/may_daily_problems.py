c9923df60a969c8b474465759212cb6dd8269f2f
L9oL9o <ertye9020@gmail.com>
2024-05-25 13:11:06 +0500

    Solved daily problem MAY-25

--- leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
+++ leetcode_tasks/DAILY_PROBLEMS/2024/5_MAY/may_daily_problems.py
@@ -466,7 +466,25 @@
 
 
 # 25 MAY
-#
+# https://leetcode.com/problems/word-break-ii/submissions/1267262440/?envType=daily-question&envId=2024-05-25
+
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+#         wordSet = set(wordDict)
+#         @cache
+#         def helper(t):
+#             combos = []
+#             if not t:
+#                 return [""]
+#             for i, _ in enumerate(t):
+#                 w = t[:i+1]
+#                 if w in wordSet:
+#                     combos.extend([
+#                         f'{w} {sentence}' if sentence else w
+#                         for sentence in helper(t[i+1:])
+#                     ])
+#             return combos
+#         return helper(s)
 
 
 # 26 MAY
